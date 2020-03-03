a4016a08446bcafdd0ed6e8c276b9831faf9f4e8
wesleykuang12 <wesleykuang12@gmail.com>
2020-03-03 02:28:28 +0000

    du ma

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
+  fetch("https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC")
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(data){
+      console.log(data);
+  });
   
 });
+
 
